02b7a7c2c43c906e8456f26a89267ca566815398
SabeshThilagar <sabesh@sabeshs-mbp.edu.ds.fhnw.ch>
2016-05-03 16:16:23 +0200

    ProjectSetting
    - rename MainClass

--- StefanWyss-SabeshThilagar/src/stefanwyss/sabeshthilagar/Main.java
+++ StefanWyss-SabeshThilagar/src/stefanwyss/sabeshthilagar/Main.java
@@ -17,7 +17,7 @@ import javafx.stage.Stage;
  *
  * @author sabesh
  */
-public class StefanWyssSabeshThilagar extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
